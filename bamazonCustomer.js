6d3a2437f1c33c19512d6b21d86a6013462c4149
aabbatiello <anthonyjabbatiello@gmail.com>
2017-06-20 21:52:21 -0400

    Function All Search working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,4 +10,53 @@ var connection = mysql.createConnection({
 });
 
 
+connection.connect(function(err) {
+	if (err) throw err;
+	queryAllData();
+  //runSearch();
+});
+
+
+function queryAllData() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price +" | " + res[i].stock_quantity);
+		}
+		console.log("-----------------------------------");
+	});
+}
+
+function runSearch() {
+	inquirer.prompt({
+		name: "action",
+		type: "list",
+		message: "What would you like to do?",
+		choices: [
+		"Find product by id",
+		"Number of units to purhase",
+		]
+	}).then(function(answer) {
+		switch (answer.action) {
+			case "Find product by id":
+				productSearch();
+				break;
+
+			case"Number of units to purhase":
+				unitsCalc();
+				break;
+		}
+	});
+}
+function productSearch() {
+	inquirer
+	.prompt({
+		name: "item_id",
+		type: "input",
+		message: "What id or product would you like to buy?"
+
+	})
+}
+
+
 
---
+++ index.js
@@ -0,0 +1,20 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: '',
+	database: 'bamazon_dB'
+});
+
+connection.connect();
+ 
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+ 
+connection.end();
+
